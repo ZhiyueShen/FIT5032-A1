1cfcb75b07b3c8987a90ce0767e11611902f014d
Zhiyue_Shen <zshe0039@student.monash.edu>
2025-10-19 07:24:00 +1100

    fix: new config file for cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    dedupe: ['vue']
-  },
-  optimizeDeps: {
-    exclude: ['firebase/app', 'firebase/auth', 'firebase/firestore']
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [vue()],
+    resolve: {
+      dedupe: ['vue']
+    },
+    optimizeDeps: {
+      exclude: ['firebase/app', 'firebase/auth', 'firebase/firestore']
+    },
+    build: {
+      commonjsOptions: {
+        transformMixedEsModules: true
+      }
+    },
+    define: {
+      'import.meta.env.VITE_MAPBOX_TOKEN': JSON.stringify(env.VITE_MAPBOX_TOKEN)
     }
   }
 })
+
